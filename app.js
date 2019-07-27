6875f19fa7e05eea7f76649343eec390b11ded3e
Dan H <spag1024@hotmail.com>
2019-07-26 20:35:42 -0500

    creating display for savings goal in dashboard

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ejs = require('express-ejs-layouts');
 const path = require('path');
-const goals = require('savingsGoalController');
+// const goals = require('savingsGoalController');
 
 const port = 3000;
 
@@ -15,6 +15,10 @@ app.use('/img', express.static(path.join(__dirname, '/public/img')));
 
 app.get('/', (req, res, next) => {
     res.render(path.join(__dirname + '/views/index'), {msg:''});
+});
+
+app.get('/dashboard', (req, res, next) => {
+    res.render(path.join(__dirname + '/views/dashboard'), {msg:''});
 });
 
 app.listen(port, console.log('listening on port ' + port));
